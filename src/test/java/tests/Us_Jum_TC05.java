a1f6da3f5ae7b2f6373ef286808f66b666e41cfc
akif <70863898+akif-ned@users.noreply.github.com>
2023-02-22 19:11:39 +0100

    jumbofilter

---
+++ src/test/java/tests/Us_Jum_TC05.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.testng.annotations.Test;
+import pages.JumboTestPage;
+import utilities.ReusableMethods;
+
+public class Us_Jum_TC05 {
+    @Test
+    public void test01() {
+        JumboTestPage jumboTestPage = new JumboTestPage();
+        // An user should able to filter by category by clicking on the filter button
+        // first you need to navogate to home page
+        ReusableMethods.cookiesHandleJumbo();
+        // to search a product send keys in the search box
+        jumboTestPage.searchBar.sendKeys("kola");
+        // assert that the category menus is displayed
+        //to do that you need to print the categories
+
+        // the locate an category element and then assert that it is displayed
+    }
+}
