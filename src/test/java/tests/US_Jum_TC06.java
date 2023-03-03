aa12e387c864b45abaf3ecd994b395db718dce62
akif <70863898+akif-ned@users.noreply.github.com>
2023-03-03 16:04:12 +0100

    last

--- src/test/java/tests/US_Jum_TC06.java
+++ src/test/java/tests/US_Jum_TC06.java
@@ -1,4 +1,14 @@
 package tests;
 
+import org.testng.annotations.Test;
+import utilities.ReusableMethods;
+
 public class US_Jum_TC06 {
+    @Test
+    public void testTC06() {
+        // handle the cookies button
+        ReusableMethods.cookiesHandleJumbo();
+        // click op the social media links
+
+    }
 }
